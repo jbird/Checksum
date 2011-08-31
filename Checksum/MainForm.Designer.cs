4a55a4594617bcecce38bccd8539cee8440ef71f
John Bird <johnrb@gmail.com>
2011-08-31 20:20:22 +0100

    Created project.

---
+++ Checksum/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Checksum {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.computeButton = new System.Windows.Forms.Button();
+            this.hashComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileLabel.Location = new System.Drawing.Point(12, 9);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(31, 13);
+            this.fileLabel.TabIndex = 0;
+            this.fileLabel.Text = "File:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(65, 6);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(207, 20);
+            this.fileTextBox.TabIndex = 1;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(278, 4);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(75, 23);
+            this.selectButton.TabIndex = 2;
+            this.selectButton.Text = "Select...";
+            this.selectButton.UseVisualStyleBackColor = true;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(12, 38);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(47, 13);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "Result:";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(65, 35);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(287, 20);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // computeButton
+            // 
+            this.computeButton.Location = new System.Drawing.Point(277, 61);
+            this.computeButton.Name = "computeButton";
+            this.computeButton.Size = new System.Drawing.Size(75, 23);
+            this.computeButton.TabIndex = 5;
+            this.computeButton.Text = "Compute";
+            this.computeButton.UseVisualStyleBackColor = true;
+            // 
+            // hashComboBox
+            // 
+            this.hashComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.hashComboBox.FormattingEnabled = true;
+            this.hashComboBox.Location = new System.Drawing.Point(200, 63);
+            this.hashComboBox.Name = "hashComboBox";
+            this.hashComboBox.Size = new System.Drawing.Size(71, 21);
+            this.hashComboBox.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 96);
+            this.Controls.Add(this.hashComboBox);
+            this.Controls.Add(this.computeButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.fileLabel);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Checksum";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button computeButton;
+        private System.Windows.Forms.ComboBox hashComboBox;
+    }
+}
+
